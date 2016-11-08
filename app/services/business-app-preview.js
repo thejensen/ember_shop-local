218cf46779a2476abdd7b384176e97a6f86f2b37
Angie Smith and Sara Jensen <angie+sara@>
2016-11-08 15:43:48 -0800

    modify the membership application, add a service for the new business info.

--- app/services/business-app-preview.js
+++ app/services/business-app-preview.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  businesses: [],
+
+  add(params) {
+    this.get('businesses').pushObject(params);
+    console.log(this.get('businesses'));
+  }
 });
