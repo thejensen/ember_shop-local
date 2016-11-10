692256550f50292fdbc2e9e5b372ac62b95f7b24
Jena SanCartier and Katy Henning <jena@>
2016-11-10 08:21:38 -0800

    style about page

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,15 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   map: Ember.inject.service('google-map'),
-
+  isShowingModal: false,
   actions: {
-    showMap(business) {
+    toggleModal(business) {
+      this.toggleProperty('isShowingModal');
       var container = this.$('.map-display')[0];
       var options = {
         center: this.get('map').center(business.get('latitude'), business.get('longitude')),
         zoom: 15
       };
       this.get('map').findMap(container, options);
+      this.set('isShowingModal', false);
     }
   }
 });
