4f5e1115f38c08517c6c676ffcf24b5b5b5d9ff4
Angie Smith <avksmit2@gmail.com>
2016-11-10 08:51:22 -0800

    change showing map to not be a modal.

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,17 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   map: Ember.inject.service('google-map'),
-  isShowingModal: false,
   actions: {
-    toggleModal(business) {
-      this.toggleProperty('isShowingModal');
+    showMap(business) {
       var container = this.$('.map-display')[0];
+      console.log(this.$('.map-display')[0]);
       var options = {
         center: this.get('map').center(business.get('latitude'), business.get('longitude')),
         zoom: 15
       };
       this.get('map').findMap(container, options);
-      this.set('isShowingModal', false);
     }
   }
 });
