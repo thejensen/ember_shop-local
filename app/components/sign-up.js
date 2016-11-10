fa8e000491ff0ed96ba60a06feaeeb7a3beefb35
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-09 16:43:47 -0800

    readded missing property from merge in sign-up.js

--- app/components/sign-up.js
+++ app/components/sign-up.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   mockAuth: Ember.inject.service(),
-
+  addBusinessButton: false,
+  
   actions: {
     signUp() {
       var params = {
